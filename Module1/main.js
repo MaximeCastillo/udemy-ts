36cdabae75e4084b8cf76df2a69aafe5e0d9e7a6
Maxime Castillo <maxime.castillo@gmail.com>
2022-11-18 16:59:40 +0100

    👷‍♂️ Setup repo

--- Module1/main.js
+++ Module1/main.js
@@ -1,11 +1,12 @@
-
+// Démonstration problème JS non typé
+// Une addition qui fait une concaténation car le type des inputs est toujours string
 
 const inputNb1 = document.querySelector("#nb1");
 const inputNb2 = document.querySelector("#nb2");
 
 document.querySelector("#calcul").addEventListener("click", () => {
   // let resultat = addition(inputNb1.value, inputNb2.value)
-  let resultat = addition(+inputNb1.value, +inputNb2.value)
+  let resultat = addition(+inputNb1.value, +inputNb2.value) // Syntaxe ES6 équivalente à parseInt()
   document.querySelector(".resultat").innerHTML = resultat
 })
 
---
+++ README.md
@@ -0,0 +1,9 @@
+Repo contenant les projets de la formation :
+
+[TypeScript : Les bases indispensables et leurs pratiques](https://www.udemy.com/course/typescript-les-bases-indispensables-et-leurs-pratiques/)
+
+#
+
+### Versions
+
+- Node : 16.17.1
