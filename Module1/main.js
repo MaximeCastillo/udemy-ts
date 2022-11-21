468d1659d1b65cc437fa0a3b6a7656d9319e8287
Maxime Castillo <maxime.castillo@gmail.com>
2022-11-21 08:07:30 +0100

    Update files to follow udemy

--- Module1/main.js
+++ Module1/main.js
@@ -1,9 +1,14 @@
 var inputNb1 = document.querySelector("#nb1");
 var inputNb2 = document.querySelector("#nb2");
 document.querySelector("#calcul").addEventListener("click", function () {
-    var resultat = addition(+inputNb1.value, +inputNb2.value);
+    var resultat = addition(inputNb1.value, inputNb2.value);
     document.querySelector(".resultat").innerHTML = resultat;
 });
 function addition(nb1, nb2) {
-    return nb1 + nb2;
+    if (typeof (nb1) === "number" && typeof (nb2) === "number") {
+        return nb1 + nb2;
+    }
+    else {
+        return "Les valeurs ne sont pas de type \"Number\"";
+    }
 }
--- Module1/main.ts
+++ Module1/main.ts
@@ -2,10 +2,14 @@ const inputNb1 = document.querySelector("#nb1");
 const inputNb2 = document.querySelector("#nb2");
 
 document.querySelector("#calcul").addEventListener("click", () => {
-  let resultat = addition(+inputNb1.value, +inputNb2.value)
+  let resultat = addition(inputNb1.value, inputNb2.value)
   document.querySelector(".resultat").innerHTML = resultat
 })
 
 function addition(nb1, nb2) {
-  return nb1 + nb2
+  if (typeof(nb1) === "number" && typeof(nb2) === "number") {
+    return nb1 + nb2
+  } else {
+    return "Les valeurs ne sont pas de type \"Number\""
+  }
 }
