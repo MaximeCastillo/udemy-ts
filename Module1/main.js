a37429e8c7b8d2caca6a12f33a1609e1e9ba6593
Maxime Castillo <maxime.castillo@gmail.com>
2022-11-21 07:59:57 +0100

    Install tsc in devDependenvies and compile file

--- Module1/main.js
+++ Module1/main.js
@@ -1,15 +1,9 @@
-// Démonstration problème JS non typé
-// Une addition qui fait une concaténation car le type des inputs est toujours string
-
-const inputNb1 = document.querySelector("#nb1");
-const inputNb2 = document.querySelector("#nb2");
-
-document.querySelector("#calcul").addEventListener("click", () => {
-  // let resultat = addition(inputNb1.value, inputNb2.value)
-  let resultat = addition(+inputNb1.value, +inputNb2.value) // Syntaxe ES6 équivalente à parseInt()
-  document.querySelector(".resultat").innerHTML = resultat
-})
-
+var inputNb1 = document.querySelector("#nb1");
+var inputNb2 = document.querySelector("#nb2");
+document.querySelector("#calcul").addEventListener("click", function () {
+    var resultat = addition(+inputNb1.value, +inputNb2.value);
+    document.querySelector(".resultat").innerHTML = resultat;
+});
 function addition(nb1, nb2) {
-  return nb1 + nb2
+    return nb1 + nb2;
 }
---
+++ Module1/main.ts
@@ -0,0 +1,11 @@
+const inputNb1 = document.querySelector("#nb1");
+const inputNb2 = document.querySelector("#nb2");
+
+document.querySelector("#calcul").addEventListener("click", () => {
+  let resultat = addition(+inputNb1.value, +inputNb2.value)
+  document.querySelector(".resultat").innerHTML = resultat
+})
+
+function addition(nb1, nb2) {
+  return nb1 + nb2
+}
