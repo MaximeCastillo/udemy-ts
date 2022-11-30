30272d401828405c12733a5fa99460139e2a7e06
Maxime Castillo <maxime.castillo@gmail.com>
2022-11-30 15:47:58 +0100

    29. Propriété dynamique d'objet

--- Module2/main.js
+++ Module2/main.js
@@ -1,40 +1,5 @@
-var prenom;
-prenom = "Matthieu";
-var age = 32;
-var sexe = true;
-var sports = ["foot", "rugby"];
-var adresse = {
-    ligne: "rue des fleurs",
-    ville: "Toulouse",
-    cp: 31000
-};
-var afficherPersonne = function (prenom, age, sexe, sports, adresse) {
-    console.log("Pr\u00E9nom : ".concat(prenom));
-    console.log("Age : ".concat(age));
-    console.log("Sexe : ".concat(sexe ? "Homme" : "Femme"));
-    for (var _i = 0, sports_1 = sports; _i < sports_1.length; _i++) {
-        var sport = sports_1[_i];
-        console.log(sport);
-    }
-    console.log("Adresse : ");
-    console.log(adresse.ligne);
-    console.log("".concat(adresse.cp, " ").concat(adresse.ville));
-};
-afficherPersonne(prenom, age, sexe, sports, adresse);
-// ES5
-// function anniversaire(age: number): number {
-//   return age + 1;
-// }
-// ES6
-var anniversaire = function (age) {
-    return age + 1;
-};
-var newAge;
-newAge = anniversaire(age);
-console.log("Age : ".concat(newAge));
-var matthieu = {
+var p1 = {
     nom: "Matthieu",
-    age: 31,
-    classe: "Guerrier",
-    sports: ["foot", "rugby"]
+    age: 31
 };
+p1.taille = 180; // Il est aussi possible dans ce cas de rajouter des propriétés de cette façon
--- Module2/main.ts
+++ Module2/main.ts
@@ -1,89 +1,11 @@
-let prenom: string;
-prenom = "Matthieu";
-let age = 32;
-let sexe = true;
-var sports = ["foot", "rugby"];
-var adresse = {
-  ligne: "rue des fleurs",
-  ville: "Toulouse",
-  cp: 31000,
-};
-
-let afficherPersonne = (
-  prenom: string,
-  age: number,
-  sexe: boolean,
-  sports: string[],
-  adresse: {
-    ligne: string;
-    ville: string;
-    cp: number;
-  }
-): void => {
-  console.log(`Prénom : ${prenom}`);
-  console.log(`Age : ${age}`);
-  console.log(`Sexe : ${sexe ? "Homme" : "Femme"}`);
-  for (var sport of sports) {
-    console.log(sport);
-  }
-  console.log("Adresse : ");
-  console.log(adresse.ligne);
-  console.log(`${adresse.cp} ${adresse.ville}`);
-};
-
-afficherPersonne(prenom, age, sexe, sports, adresse);
-
-// ES5
-// function anniversaire(age: number): number {
-//   return age + 1;
-// }
-
-// ES6
-let anniversaire = (age: number): number => {
-  return age + 1;
-};
-
-let newAge: number;
-newAge = anniversaire(age);
-console.log(`Age : ${newAge}`);
-
-/// 27. L'intersection
-
-// Avec les "types"
-// type Personnage = {
-//   nom: string;
-//   age: number;
-// };
-
-// type Humain = {
-//   classe: string;
-// };
-
-// type Homme = Personnage & Humain;
-
-// const matthieu: Homme = {
-//   nom: "Matthieu",
-//   age: 31,
-//   classe: "Guerrier"
-// };
-
-// Avec les "interfaces"
-interface Personnage {
+type Personnage = {
   nom: string;
-  age: number;
-}
+  [props: string]: string | number; // On pourra rajouter d'autres propriétés, tant qu'elles sont de type string ou number
+};
 
-interface Humain {
-  classe: string;
-}
-
-interface Homme extends Personnage, Humain {
-  sports: string[];
-}
-
-const matthieu: Homme = {
+const p1: Personnage = {
   nom: "Matthieu",
   age: 31,
-  classe: "Guerrier",
-  sports: ["foot", "rugby"],
 };
+
+p1.taille = 180; // Il est aussi possible dans ce cas de rajouter des propriétés de cette façon
