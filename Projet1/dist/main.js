98764a5abf7b4d78dfb6d5fa694a5df00a3d1aff
Maxime Castillo <maxime.castillo@gmail.com>
2023-01-11 11:03:27 +0100

    39. Le type DeviseType et les objets

--- Projet1/dist/main.js
+++ Projet1/dist/main.js
@@ -1,4 +1,28 @@
 "use strict";
-const app = document.querySelector("#app");
-app.innerHTML = "<h1>Coucou</h1>";
+const dollar = {
+    nom: "Dollar",
+    code: "Dol",
+    symbole: "$",
+    taux: 1,
+};
+const euro = {
+    nom: "Euro",
+    code: "Eur",
+    symbole: "€",
+    taux: 1.2,
+};
+const livre = {
+    nom: "Livre",
+    code: "Liv",
+    symbole: "£",
+    taux: 1.39,
+};
+const yuan = {
+    nom: "Yuan",
+    code: "Yua",
+    symbole: "&yen;",
+    taux: 0.15,
+};
+const devises = [dollar, euro, livre, yuan];
+console.log(devises);
 //# sourceMappingURL=main.js.map
--- Projet1/src/main.ts
+++ Projet1/src/main.ts
@@ -1,2 +1,38 @@
-const app = document.querySelector("#app")!
-app.innerHTML = "<h1>Coucou</h1>"
+type DeviseType = {
+  nom: string;
+  code: string;
+  symbole: string;
+  taux: number; // Taux de conversion vers le dollar
+};
+
+const dollar: DeviseType = {
+  nom: "Dollar",
+  code: "Dol",
+  symbole: "$",
+  taux: 1,
+};
+
+const euro: DeviseType = {
+  nom: "Euro",
+  code: "Eur",
+  symbole: "€",
+  taux: 1.2,
+};
+
+const livre: DeviseType = {
+  nom: "Livre",
+  code: "Liv",
+  symbole: "£",
+  taux: 1.39,
+};
+
+const yuan: DeviseType = {
+  nom: "Yuan",
+  code: "Yua",
+  symbole: "&yen;",
+  taux: 0.15,
+};
+
+const devises: DeviseType[] = [dollar, euro, livre, yuan];
+
+console.log(devises);
